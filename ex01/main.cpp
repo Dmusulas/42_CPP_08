6ae126daec7c46a9fbd9262adf35339cfd506f77
Dovi <dmusulas@student.42berlin.de>
2025-11-29 21:23:17 +0100

    bla

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,14 @@
 /*   By: dmusulas <dmusulas@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 16:59:01 by dmusulas          #+#    #+#             */
-/*   Updated: 2025/11/28 16:59:25 by dmusulas         ###   ########.fr       */
+/*   Updated: 2025/11/29 20:21:01 by dmusulas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
     Span sp = Span(5);
@@ -21,5 +24,36 @@ int main() {
     sp.addNumber(11);
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
+
+    // testing with a looooot of numbers
+    unsigned int size = 10 * static_cast<unsigned int>(std::pow(10, 6));
+    std::cout << "Size of span: " << size << std::endl;
+
+    Span bigspan = Span(size);
+    std::vector<int> randomNumbers;
+    randomNumbers.reserve(size); // Optimization to prevent reallocations
+
+    std::srand(std::time(NULL));
+    for (unsigned int i = 0; i < size; i++) {
+        randomNumbers.push_back(std::rand());
+    }
+
+    try {
+        bigspan.addNumber(randomNumbers.begin(), randomNumbers.end());
+    } catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Shortest Span: " << bigspan.shortestSpan() << std::endl;
+    std::cout << "Longest Span: " << bigspan.longestSpan() << std::endl;
+
+    // now make it throw error
+    try {
+        bigspan.addNumber(1);
+    } catch (std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: dmusulas <dmusulas@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 20:03:27 by dmusulas          #+#    #+#             */
-/*   Updated: 2025/11/28 20:30:16 by dmusulas         ###   ########.fr       */
+/*   Updated: 2025/11/29 20:31:09 by dmusulas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,14 @@
 
 #include <exception>
 #include <iostream>
-#include <stack>
 #include <list>
+#include <stack>
 
 template <class T> class MutantStack : public std::stack<T> {
 private:
-    // Attributes
-
 public:
-    MutantStack() : std::stack<T>() {};
-    MutantStack(const MutantStack &other) : std::stack<T>(other) {};
+    MutantStack() : std::stack<T>(){};
+    MutantStack(const MutantStack &other) : std::stack<T>(other){};
     MutantStack &operator=(const MutantStack &other) {
         if (this != &other) {
             std::stack<T>::operator=(other);
@@ -38,15 +36,23 @@ public:
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
 
-    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef
+        typename std::stack<T>::container_type::const_iterator const_iterator;
 
     const_iterator begin() const { return this->c.begin(); }
     const_iterator end() const { return this->c.end(); }
 
-	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator
+        reverse_iterator;
 
-	reverse_iterator rbegin() { return this->c.rbegin(); }
-	reverse_iterator rend() { return this->c.rend(); }
+    reverse_iterator rbegin() { return this->c.rbegin(); }
+    reverse_iterator rend() { return this->c.rend(); }
+
+    typedef typename std::stack<T>::container_type::const_reverse_iterator
+        const_reverse_iterator;
+
+    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+    const_reverse_iterator rend() const { return this->c.rend(); }
 };
 
 #endif
